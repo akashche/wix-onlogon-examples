854ea9c2039529f73ff3789f7821ff2d31f1722c
akashche <akashche@redhat.com>
2017-01-30 07:25:25 -0800

    deferred action example

--- build.bat
+++ build.bat
@@ -10,21 +10,27 @@ if exist "%SCRIPT_DIR%work" exit /b 1
 mkdir "%SCRIPT_DIR%work" || exit /b 1
 
 rem test01
-pushd "%SCRIPT_DIR%work" || exit /b 1
-..\wix\bin\candle.exe ..\test01.wxs || exit /b 1
-..\wix\bin\light.exe -ext WixUIExtension -ext WixUtilExtension -sw1076 test01.wixobj || exit /b 1
-popd || exit /b 1
+rem pushd "%SCRIPT_DIR%work" || exit /b 1
+rem ..\wix\bin\candle.exe ..\test01.wxs || exit /b 1
+rem ..\wix\bin\light.exe -ext WixUIExtension -ext WixUtilExtension -sw1076 test01.wixobj || exit /b 1
+rem popd || exit /b 1
 
 rem test02
-pushd "%SCRIPT_DIR%work" || exit /b 1
-..\wix\bin\candle.exe ..\test02.wxs || exit /b 1
-..\wix\bin\light.exe -ext WixUIExtension -ext WixUtilExtension -sw1076 test02.wixobj || exit /b 1
-popd || exit /b 1
+rem pushd "%SCRIPT_DIR%work" || exit /b 1
+rem ..\wix\bin\candle.exe ..\test02.wxs || exit /b 1
+rem ..\wix\bin\light.exe -ext WixUIExtension -ext WixUtilExtension -sw1076 test02.wixobj || exit /b 1
+rem popd || exit /b 1
 
 rem test03
+rem pushd "%SCRIPT_DIR%work" || exit /b 1
+rem ..\wix\bin\candle.exe ..\test03.wxs || exit /b 1
+rem ..\wix\bin\light.exe -ext WixUIExtension -ext WixUtilExtension -sw1076 test03.wixobj || exit /b 1
+rem popd || exit /b 1
+
+rem test04
 pushd "%SCRIPT_DIR%work" || exit /b 1
-..\wix\bin\candle.exe ..\test03.wxs || exit /b 1
-..\wix\bin\light.exe -ext WixUIExtension -ext WixUtilExtension -sw1076 test03.wixobj || exit /b 1
+..\wix\bin\candle.exe ..\test04.wxs || exit /b 1
+..\wix\bin\light.exe -ext WixUIExtension -ext WixUtilExtension -sw1076 test04.wixobj || exit /b 1
 popd || exit /b 1
 
 echo EXIT_BUILD_SUCCESS
--- run.bat
+++ run.bat
@@ -5,21 +5,27 @@ set BAD_SLASH_SCRIPT_DIR=%~dp0
 set SCRIPT_DIR=%BAD_SLASH_SCRIPT_DIR:\=/%
 
 rem test01
-pushd "%SCRIPT_DIR%work" || exit /b 1
-msiexec /q /i test01.msi /l*v test01_log.txt || exit /b 1
-msiexec /q /x test01.msi || exit /b 1
-popd || exit /b 1
+rem pushd "%SCRIPT_DIR%work" || exit /b 1
+rem msiexec /q /i test01.msi /l*v test01_log.txt || exit /b 1
+rem msiexec /q /x test01.msi || exit /b 1
+rem popd || exit /b 1
 
 rem test02
-pushd "%SCRIPT_DIR%work" || exit /b 1
-msiexec /q /i test02.msi /l*v test02_log.txt || exit /b 1
-msiexec /q /x test02.msi || exit /b 1
-popd || exit /b 1
+rem pushd "%SCRIPT_DIR%work" || exit /b 1
+rem msiexec /q /i test02.msi /l*v test02_log.txt || exit /b 1
+rem msiexec /q /x test02.msi || exit /b 1
+rem popd || exit /b 1
 
 rem test03
+rem pushd "%SCRIPT_DIR%work" || exit /b 1
+rem msiexec /q /i test03.msi /l*v test03_log.txt || exit /b 1
+rem msiexec /q /x test03.msi || exit /b 1
+rem popd || exit /b 1
+
+rem test04
 pushd "%SCRIPT_DIR%work" || exit /b 1
-msiexec /q /i test03.msi /l*v test03_log.txt || exit /b 1
-msiexec /q /x test03.msi || exit /b 1
+msiexec /q /i test04.msi /l*v test04_log.txt || exit /b 1
+msiexec /q /x test04.msi || exit /b 1
 popd || exit /b 1
 
 echo EXIT_RUN_SUCCESS
